cc7fc3e2a7f93e01ff176ca0590a457c8ceedc6d
raissaazaria <91534032+raissaazaria@users.noreply.github.com>
2021-10-16 19:49:46 +0700

    Update python exercise 1 no 2.py

--- python exercise 1 no 2.py
+++ python exercise 1 no 2.py
@@ -1,6 +1,6 @@
-v=int(input("Enter the plane's take off speed in m/s:"))            #Input
+v=int(input("Enter the plane's take off speed in m/s:"))                                                        #Enter the statements using the unit
 a=float(input("Enter the plane's acceleration m/s**2:"))
 
-formula = (v**2)/(2*a)
+formula = (v**2)/(2*a)                                                                                          #Enter the formula based on the question
 
-print("The minimum runway length needed for this airplane is", formula,"meters" )
+print("The minimum runway length needed for this airplane is", formula,"meters" )                               #Print the results then add the formula and the unit
