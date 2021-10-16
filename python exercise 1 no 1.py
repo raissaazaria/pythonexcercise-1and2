3440b9ff4f1262fb474b73af4f57e8c6c3655ab8
raissaazaria <91534032+raissaazaria@users.noreply.github.com>
2021-10-16 19:46:08 +0700

    Update python exercise 1 no 1.py

--- python exercise 1 no 1.py
+++ python exercise 1 no 1.py
@@ -1,7 +1,8 @@
-x1 = eval(input("Enter the x-coordinate for point 1 : "))
+x1 = eval(input("Enter the x-coordinate for point 1 : "))                                                            #Enter statetment for each coordinate
 y1 = eval(input("Enter the y-coordinate for point 1 : "))
 x2 = eval(input("Enter the x-coordinate for point 2 : "))
 y2 = eval(input("Enter the y-coordinate for point 2 : "))
-slope = (y2 - y1) / (x2 - x1)
 
-print("The slope that connects", (x1, y1), "and", (x2, y2), "is", format(slope, '.5f'))
+slope = (y2 - y1) / (x2 - x1)                                                                                        #Enter formula based on the question
+
+print("The slope that connects", (x1, y1), "and", (x2, y2), "is", format(slope, '.5f'))                              #Print the the slope and the rounding
